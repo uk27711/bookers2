fea045d52f07b89cb50343900bdf4e6ba4e0c5fb
uk27711 <ec2-user@ip-172-31-14-99.ap-northeast-1.compute.internal>
2025-02-06 11:17:33 +0000

    20250206

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "lists/new"
+  get "lists/index"
+  get "lists/show"
+  get "lists/edit"
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about', to: 'homes#about', as: :about
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:show, :edit, :update]
 end
